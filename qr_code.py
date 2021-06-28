d1532f7476b779a53790c546ee9112198d019f6d
Goto Norio <gotonorio@gmail.com>
2021-06-28 11:37:17 +0900

    保存ファイル名の間違いを間違いとして処理

--- qr_code.py
+++ qr_code.py
@@ -5,32 +5,25 @@ import sys
 
 import qrcode
 
-DEFAULT_FILE_NAME = 'default_qr_code.png'
-
 
 def make_qrcode(qr_string, file_name):
-    """ qrcodeライブラリを利用
-    - 与えられたfile_nameで保存出来なければソースのあるディレクトリに保存する。
-    """
-    path_sep = os.sep
+    """ qrcodeライブラリを利用 """
     img = qrcode.make(qr_string)
-    current_dir = os.getcwd()
     try:
         img.save(file_name)
+        img_address = "{}".format(file_name)
     except FileNotFoundError:
-        print('{}が間違っています'.format(file_name))
-        file_name = current_dir + path_sep + DEFAULT_FILE_NAME
-        img.save(file_name)
-    img_address = "{}".format(file_name)
-    return img_address
+        img_address = None
+    return img_address, img
 
 
 if __name__ == "__main__":
     args = sys.argv
     len_args = len(args)
     if len_args == 1:
-        file_name = 'qr_code.png'
-        qr_string = input()
+        path_sep = os.sep
+        current_dir = os.getcwd()
+        file_name = current_dir + path_sep + 'qr_code.png'
     elif len_args == 2:
         file_name = args[1]
     else:
@@ -38,5 +31,9 @@ if __name__ == "__main__":
         sys.exit()
     print("QRコードに変換したい文字列を入力してください: ", end="")
     qr_string = input()
-    file_address = make_qrcode(qr_string, file_name)
-    print("「{}」にQRコード画像を保存しました".format(file_address))
+    img_address, img = make_qrcode(qr_string, file_name)
+    if img_address:
+        print("「{}」にQRコード画像を保存しました".format(img_address))
+        img.show()
+    else:
+        print('{}が間違っています'.format(file_name))
